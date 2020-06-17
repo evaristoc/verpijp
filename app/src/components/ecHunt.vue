3ecb3b7e11c7e604f03484bfe91fff3ed3e29a03
ec <varikvi@yahoo.com>
2020-06-17 11:56:27 +0200

    unit01:sarphati: validation implementation

--- app/src/components/ecHunt.vue
+++ app/src/components/ecHunt.vue
@@ -1,23 +1,46 @@
 <template>
     <div>
-        <h3>HELLO HUNT</h3>
+        <h3>Tweede van der Helststraat (between 3-13)</h3>
         <div class="container">
             <div class="row mt-1">
                 <div class="col-6">
                     <img src="https://i.ibb.co/zbF6Kt9/8232820489-579018c1cd-b.jpg" class="float-left" width="100%">
                 </div>
             </div>
-            <div class="row mt-1">
+            <!--<div class="row mt-1">
                 <div class="col-12">
-                    Tweede van der Helststraat (between 3-13): What is the name of the business with this window?
+                    What is the name of the business with this window?
                 </div>
-            </div>
+            </div>-->
             <div class="row mt-1">
-                <div class="col-7">
-                    <form>
-                         <input type="text" class="form-control" id="entry">
+                <div class="col-7" role="group">
+                        <label for="input-live"> What is the name of the business with this window?</label>
+                        <b-form-input
+                          id="input-live"
+                          v-model="name"
+                          :state="nameState"
+                          aria-describedby="input-live-help input-live-feedback"
+                          placeholder="Enter your name"
+                          trim
+                        ></b-form-input>
+                    
+                        <!-- This will only be shown if the preceding input has an invalid state -->
+                        <b-form-invalid-feedback id="input-live-feedback">
+                          Enter at least 3 letters
+                        </b-form-invalid-feedback>
+                    
+                        <!-- This is a form text block (formerly known as help block) -->
+                        <b-form-text id="input-live-help">Your full name.</b-form-text>
+                    <!--<form>
+                         <input type="text" class="form-control" id="entry" pattern="GlouGlou" required>
+                        <div class="valid-feedback">
+                         Looks good!
+                       </div>
+                       <div class="invalid-feedback">
+                         Doesn't look good!
+                       </div>
                          <button class="btn btn-success" type="submit">Solution</button>
-                    </form>
+                    </form>-->
                 </div>
             </div>
         </div>
