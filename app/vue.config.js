f54daea52569a960d95eb9633db4d7f78dbd162e
ec <varikvi@yahoo.com>
2020-07-16 17:11:40 +0200

    webpack: changes in vue.config

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,3 +1,25 @@
+//https://programming.vip/docs/how-to-configure-vue-config.js-when-creating-a-project-with-vue-cli3.html
 module.exports = {
-outputDir:'../dist',
+  outputDir:'../heroku/dist',
+  assetsDir: './assets/',
+  filenameHashing: false,
+
+  // Modify Webpack config
+  // https://cli.vuejs.org/config/#chainwebpack
+  chainWebpack: config => {
+    // Not naming bundle 'app'
+    config.entryPoints.delete('app');
+  },
+
+  // Overriding webpack config
+  configureWebpack: {
+    optimization: {
+      splitChunks: false
+    },
+    // Naming bundle 'build'
+    entry: {
+      build: './src/main.js'
+    }
+  }
+
 }
